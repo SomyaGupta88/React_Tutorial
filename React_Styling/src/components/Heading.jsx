35554a3edebf9b3f96c994f99f244739a81f7780
Somya Gupta <52820821+SomyaGupta88@users.noreply.github.com>
2020-09-17 23:03:20 +0530

    implement react styling with components

--- React_Styling/src/components/Heading.jsx
+++ React_Styling/src/components/Heading.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-const date = new Date();
-//console.log(date.getHours());
-const hour = date.getHours();
-
-let greet;
-const customStyle = {
-  color: ""
-};
-if (hour < 12) {
-  greet = "Good morning";
-  customStyle.color = "red";
-} else if (hour >= 18) {
-  greet = "Good evening";
-  customStyle.color = "green";
-} else {
-  greet = "Good aftenoon";
-  customStyle.color = "blue";
-}
-
 function Heading() {
+  const date = new Date();
+  //console.log(date.getHours());
+  const hour = date.getHours();
+
+  let greet;
+  const customStyle = {
+    color: ""
+  };
+  if (hour < 12) {
+    greet = "Good morning";
+    customStyle.color = "red";
+  } else if (hour >= 18) {
+    greet = "Good evening";
+    customStyle.color = "green";
+  } else {
+    greet = "Good aftenoon";
+    customStyle.color = "blue";
+  }
+
   return (
     <h1 className="heading" style={customStyle}>
       {greet}
