624d51f9b549558283bfd1d8c9471eed86f84779
Somya Gupta <52820821+SomyaGupta88@users.noreply.github.com>
2020-09-25 21:38:14 +0530

    Add files via upload

---
+++ keeper-app-part-1/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Note from "./Note.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ keeper-app-part-1/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>Heading</h1>
+      <p>sub</p>
+    </div>
+  );
+}
+export default Note;
