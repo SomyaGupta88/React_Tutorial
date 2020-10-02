2dec0f5718d5a12a253731aec8cd68160ddc5ce5
Somya Gupta <52820821+SomyaGupta88@users.noreply.github.com>
2020-10-02 18:22:47 +0530

    Add files via upload

---
+++ introduction-to-state-completed/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function strike() {
+  document.getElementById("root").style.textDecoration = "line-through";
+}
+
+function unStrike() {
+  document.getElementById("root").style.textDecoration = null;
+}
+
+function App() {
+  return (
+    <div>
+      <p>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
+    </div>
+  );
+}
+
+export default App;
